39ab556c9220fdb2c730149c854e18f18b7bcb97
shristigupta1707-ops <shristigupta1707@gmail.com>
2026-09-10 21:44:36 +0530

    https://tastybites-backend-48or.onrender.com

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create one Axios instance for our backend
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: "https://tastybites-backend-48or.onrender.com"
 });
 
 export default api;
